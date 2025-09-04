60b9b6fab851d92244c71cf12eb188f81b27eed9
mheavey2 <mheavey2@gmail.com>
2025-09-04 18:48:06 +0100

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -9,11 +9,21 @@ import {
 } from "react-router-dom";
 
 import App from "./components/App/App.jsx";
+import About from "./components/About/About.jsx";
+import Team from "./components/Team/Team.jsx";
+import Classes from "./components/Classes/Classes.jsx";
+import Membership from "./components/Membership/Membership.jsx";
+import Contact from "./components/Contact/Contact.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/classes" element={<Classes />} />
+      <Route path="/membership" element={<Membership />} />
+      <Route path="/contact" element={<Contact />} />
     </>
   )
 );
