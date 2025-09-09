8e1194b9da614fc906e9eb83ceb19516fbecac10
mheavey2 <mheavey2@gmail.com>
2025-09-09 12:57:25 +0100

    add navbar menu styling

--- src/components/NavbarMenu/NavbarMenu.jsx
+++ src/components/NavbarMenu/NavbarMenu.jsx
@@ -4,12 +4,18 @@ import styles from "./NavbarMenu.module.css";
 const NavbarMenu = () => {
   return (
     <div className={styles.navbarMenuOuter}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About Us</NavLink>
-      <NavLink to="/team">The Team</NavLink>
-      <NavLink to="/classes">Classes</NavLink>
-      <NavLink to="/membership">Membership</NavLink>
-      <NavLink to="/contact">Contact Us</NavLink>
+      <div className={styles.navbarExitContainer}>
+        <img src="gold-x.svg" alt="gold x exit menu icon" />
+      </div>
+
+      <div className={styles.navbarMenuLinks}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About Us</NavLink>
+        <NavLink to="/team">The Team</NavLink>
+        <NavLink to="/classes">Classes</NavLink>
+        <NavLink to="/membership">Membership</NavLink>
+        <NavLink to="/contact">Contact Us</NavLink>
+      </div>
     </div>
   );
 };
