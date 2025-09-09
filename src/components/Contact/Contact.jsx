0328a3de628ea0591b5bbecb319530b2256db63d
mheavey2 <mheavey2@gmail.com>
2025-09-09 18:24:54 +0100

    add contact details and styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,27 @@
+import styles from "./Contact.module.css";
+
 const Contact = () => {
-  return <div>Contact Placeholder</div>;
+  return (
+    <div className={styles.contactOuter}>
+      <div className={styles.infoContainer}>
+        <h3>Location</h3>
+        <p>Unit 1</p>
+        <p>Business Park</p>
+        <p>Limerick</p>
+      </div>
+      <div className={styles.phoneContainer}>
+        <h3>Phone</h3>
+        <p>061-1234567</p>
+      </div>
+
+      <div className={styles.hoursContainer}>
+        <h3>Opening Hours</h3>
+        <p>Mon-Sat: 06:00-22:00</p>
+
+        <p>Sun: 08:00-20:00</p>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
