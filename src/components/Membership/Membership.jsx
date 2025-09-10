85f074595ea548ec6c3ac337dac34dd0502d0252
mheavey2 <mheavey2@gmail.com>
2025-09-10 12:36:01 +0100

    add membership content and styling

--- src/components/Membership/Membership.jsx
+++ src/components/Membership/Membership.jsx
@@ -1,4 +1,45 @@
+import styles from "./Membership.module.css";
+
 const Membership = () => {
-  return <div>Membership Placeholder</div>;
+  return (
+    <div className={styles.membershipContainer}>
+      <div className={styles.introOfferOuter}>
+        <h3>Introductory Offer</h3>
+        <p>€199</p>
+        <ul>
+          <li>Unlimited access to facilities </li>
+          <li>15 Class Pass per month</li>
+          <li>10% off renewal</li>
+        </ul>
+      </div>
+
+      <div className={styles.membershipOuter}>
+        <div className={styles.membershipItem} id={styles.membershipOne}>
+          <h3>Tier 1</h3>
+          <p>€249</p>
+          <ul>
+            <li>Unlimited access to facilities </li>
+            <li>Unlimited Class Pass</li>
+          </ul>
+        </div>
+        <div className={styles.membershipItem} id={styles.membershipTwo}>
+          <h3>Tier 2</h3>
+          <p>€175</p>
+          <ul>
+            <li>Unlimited access to facilities </li>
+            <li>10 Class Pass per month</li>
+          </ul>
+        </div>
+        <div className={styles.membershipItem} id={styles.membershipThree}>
+          <h3>Tier 3</h3>
+          <p>€99</p>
+          <ul>
+            <li>Unlimited access to facilities </li>
+            <li>4 Class Pass per month</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Membership;
