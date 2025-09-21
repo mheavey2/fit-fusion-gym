1c5dde0e5f5e3c4be168bb113213594e168a49a9
mheavey2 <mheavey2@gmail.com>
2025-09-21 17:39:53 +0100

    add aria roles and labels and testid attributes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,26 +2,37 @@ import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
-    <div className={styles.contactOuter} role="contactinfo">
-      <div className={styles.infoContainer} role="group" aria-label="location">
+    <div
+      className={`${styles.contactOuter}, contactContainer`}
+      role="contactinfo"
+      data-testid="contact-outer"
+    >
+      <div
+        className={`${styles.infoContainer}, infoContainer`}
+        role="group"
+        aria-label="location"
+        data-testid="info-container"
+      >
         <h3>Location</h3>
         <p>Unit 1</p>
         <p>Business Park</p>
         <p>Limerick</p>
       </div>
       <div
-        className={styles.phoneContainer}
+        className={`${styles.phoneContainer}, phoneContainer`}
         role="group"
         aria-label="Contact Information"
+        data-testid="phone-container"
       >
         <h3>Phone</h3>
         <p>061-1234567</p>
       </div>
 
       <div
-        className={styles.hoursContainer}
+        className={`${styles.hoursContainer}, hoursContainer`}
         role="group"
         aria-label="Business Hours"
+        data-testid="hours-container"
       >
         <h3>Opening Hours</h3>
         <p>Mon-Sat: 06:00-22:00</p>
